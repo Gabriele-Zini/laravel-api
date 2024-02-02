43bf7b05b85eb66e5a14234dc5052aefed0c2665
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-02 17:19:51 +0100

    add: api/controller show function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,11 +11,42 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::with(['type', 'technologies', 'user'])->paginate(10);
-        return response()->json(
-            [
-                'result' => $projects,
-                'success' => true,
-            ]
-        );
+
+        if ($projects) {
+            return response()->json(
+                [
+                    'result' => $projects,
+                    'success' => true,
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Nessun progetto trovato'
+                ]
+            );
+        }
+    }
+
+    public function show(string $slug)
+    {
+        $project = Project::with(['type', 'technologies', 'user'])->where('slug', $slug)->first();
+        if ($project) {
+
+            return response()->json(
+                [
+                    'result' => $project,
+                    'success' => true,
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Il progetto non esiste'
+                ]
+            );
+        }
     }
 }
