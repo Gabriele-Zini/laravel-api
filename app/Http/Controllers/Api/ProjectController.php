70814e310e27fc7774368fc2f2abaead361e9d6a
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-02 13:10:32 +0100

    update index function in api project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with(['type'])->paginate(10);
+        $projects = Project::with(['type', 'technologies', 'user'])->paginate(10);
         return response()->json(
             [
                 'result' => $projects,
